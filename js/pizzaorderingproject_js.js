fa4296d8b8d4ee32e0fd597e33c844cfc31e320c
Ana Meyer <ana.marie.meyer@gmail.com>
2018-11-25 17:51:14 -0800

    final project

--- js/pizzaorderingproject_js.js
+++ js/pizzaorderingproject_js.js
@@ -1,7 +1,7 @@
 function getReceipt() {
 	// This initializes our string so it can get passed from  
 	// function to function, growing line by line into a full receipt
-	var text1 = "<h3>You Ordered:</h3>";
+	var text1 = "<h3>Your Order:</h3>";
 	var runningTotal = 0;
 	var sizeTotal = 0;
 	var sizeArray = document.getElementsByClassName("size");
